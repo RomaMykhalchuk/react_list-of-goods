9636a766590e09c4a3567c009eafdb73dfc272b6
Roma <roman.mykhalchuk@yahoo.com>
2020-04-30 00:33:58 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.44c22efbaae5b9a084b6e125967f2d5e.js"
 );
 
 self.addEventListener('message', (event) => {
